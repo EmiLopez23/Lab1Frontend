3a74c0c39ed5b9b44f86960f7ec157dd4b63d8d6
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-03-23 16:30:57 -0300

    register verification

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -10,34 +10,36 @@ export default function Register(){
     const[error,setError]=useState(null)
 
 
-    function handleSubmit(event){
+    async function handleSubmit(event){
         event.preventDefault()
         const user={username,password,email}
         try{
-            fetch("http://localhost:8080/register", {
+            const response = await fetch("http://localhost:8080/register", {
                 method: "POST",
                 headers: {
                   "Content-Type": "application/json",
                 },
                 body: JSON.stringify(user),
-              }).then(resp=>{
-                if (!resp.ok) {
-                    throw new Error(resp.text());
-                  }
-                navigate("/login");
               })
 
+            if (!response.ok) {
+                throw new Error(await response.text());
+                }
+            const data = await response.json();
+            localStorage.setItem("token",data.token)
+            navigate("/")
               
         }catch(error){
             setError(error.message)
         }
     }
-
+    
     return <div className="body"> 
         <div className="card">
             <div className="title">
                 <h1>REGISTER</h1>
             </div>
+            {error && <div className="error-div">{error}</div>}
             <form onSubmit={handleSubmit}>
             <input
                     type="email"
