202b758a535faab4c889c6a1999567197be9b4bb
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-04-26 03:00:57 -0300

    add Game To User Inventory

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -5,12 +5,14 @@ import { UserContext } from "../../contexts/UserContext";
 import AddItem from "../../components/AddItem/AddItem";
 import PopUpContainer from "../../components/PopUpContainer/PopUpContainer";
 import AddGame from "../../components/AddGame/AddGame";
+import AddToInventory from "../../components/AddToInventory/AddToInventory";
 
 export default function Inventory(){
     const {token,role} = useContext(UserContext)
     const [items,setItems] = useState([])
     const [addItem,toggleItem] = useState(false)
     const [addGame,toggleGame] = useState(false)
+    const [addToInventory,toggleToInventory] = useState(false)
 
     useEffect(()=>{
         fetch("http://localhost:8080/inventory/all", {
@@ -21,6 +23,15 @@ export default function Inventory(){
             setItems(data)
         })
     },[token])
+
+    
+    function openItemToInventoryAdder(){
+        toggleToInventory(true)
+    }
+
+    function closeItemToInventoryAdder(){
+        toggleToInventory(false)
+    }
 
     function openItemGenerator(){
         toggleItem(true)
@@ -44,19 +55,26 @@ export default function Inventory(){
         <div>
             <h1 className="text-light">Inventory</h1>
         </div>
-        {role==="ADMIN" &&
+        
         <div className="d-flex justify-content-end gap-2">
             <div className="">
-                <button onClick={openGameGenerator} className="btn add-btn">Add Game</button>
+                <button onClick={openItemToInventoryAdder} className="btn add-btn">Add Item to Inventory</button>
             </div>
-            <div className="">
-                <button onClick={openItemGenerator} className="btn add-btn">Add New Item</button>
-            </div>
-        </div>}
+            {role==="ADMIN" && <>
+                <div className="">
+                    <button onClick={openGameGenerator} className="btn add-btn">Add Game</button>
+                </div>
+                <div className="">
+                    <button onClick={openItemGenerator} className="btn add-btn">Add New Item</button>
+                </div>
+            </>
+            }
+        </div>
         <div className="items-container p-3">
             {items.map(item=><ItemCard key={item.name} item={item}/>)}
         </div>
         </div>
+        {addToInventory && <PopUpContainer element={<AddToInventory/>} onClick={closeItemToInventoryAdder}/>}
         {addItem && <PopUpContainer element={<AddItem/>} onClick={closeItemGenerator} />}
         {addGame && <PopUpContainer element={<AddGame/>} onClick={closeGameGenerator} />}
     </> 
