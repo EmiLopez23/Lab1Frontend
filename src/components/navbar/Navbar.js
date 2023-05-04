d6b529eee8f1bd395d7256d9baef92fd416db729
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-05-04 02:59:44 -0300

    logito puesto

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,19 @@
 import "./navbar.css"
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export default function Navbar(){
-
+    const navigate = useNavigate()
     const {logout} = useContext(UserContext)
+    const logo = "logoTradePal.png"
     
 
     return <div className="navbar navbar-expand-lg bg-dark p-3 justify-content-between">
     <div className="items-container">
+      <img src={logo} alt="logo" className="logo" onClick={()=>navigate("/",{replace:true})}/>
       <ul className="navbar-items">
-        <li className="navbar-li"><Link className="navbar-link" to={"/"}>Home</Link></li>
         <li className="navbar-li"><Link className="navbar-link" to={"/inventory"}>Inventory</Link></li>
       </ul>
             
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,6 @@ import "./Home.css"
 
 export default function Home(){
 
-
-
     return <div className="home mt-4 vh-100 d-flex align-items-center justify-content-center">
             <h1 className="text-light">Coming Soon...</h1>
             </div>
