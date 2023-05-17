717b1a49c8634098e51c6c7819136278f6965cee
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-05-17 11:11:07 -0300

    img resource path fix

--- src/components/ItemCard/ItemImg.js
+++ src/components/ItemCard/ItemImg.js
@@ -2,6 +2,6 @@
 
 export default function ItemImg({item,width,onClick}){
     return <div onClick={onClick}>
-        <img src={`http://localhost:8080/${item.imgPath}`} className="rounded-1 item-img" style={{width:width, height:"auto", backgroundColor:"black"}} alt={item.name}></img>
+        <img src={`http://localhost:8080/resources/${item.imgPath}`} className="rounded-1 item-img" style={{width:width, height:"auto", backgroundColor:"black"}} alt={item.name}></img>
     </div>
 }
