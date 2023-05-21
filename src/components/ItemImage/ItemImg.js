8a36c15cea3b65f0fcdca77927ddca031faeea20
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-05-21 02:10:39 -0300

    item img optimized

--- src/components/ItemImage/ItemImg.js
+++ src/components/ItemImage/ItemImg.js
@@ -1,7 +1,19 @@
 
 
 export default function ItemImg({item,width,onClick}){
+    let style = {
+        width:width, 
+        height:"auto", 
+        backgroundColor:"black",
+        cursor: onClick ? "pointer" : "default"
+    }
+
     return <div onClick={onClick}>
-        <img src={`http://localhost:8080/resources/${item.imgPath}`} className="rounded-1 item-img" style={{width:width, height:"auto", backgroundColor:"black"}} alt={item.name}></img>
+        <img 
+            src={`http://localhost:8080/resources/${item.imgPath}`} 
+            className="rounded-1 item-img" 
+            style={style} 
+            alt={item.name}
+        />
     </div>
 }
