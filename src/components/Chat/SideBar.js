3528a0be971b19215127b5455692e3c9bfb05fa7
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-06-14 00:42:13 -0300

    notifications on sidebar

--- src/components/Chat/SideBar.js
+++ src/components/Chat/SideBar.js
@@ -4,11 +4,37 @@ import ApiService from "../../services/ApiService"
 import { useNavigate } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import SockJS from "sockjs-client"
+import { over } from "stompjs"
 
 export default function SideBar({username,token, id}){
     const [contacts,setContacts] = useState([])
+    const [notification,setNotifications] = useState({})
+    const [stompClient, setStompClient] = useState(null);
     const navigate = useNavigate()
 
+    useEffect(()=>{
+        let stomp = null;
+
+      const connectToWebSocket = () => {
+        const socket = new SockJS('http://localhost:8080/ws');
+        stomp = over(socket);
+      
+        stomp.connect({}, () => {
+          setStompClient(stomp);
+        });
+      };
+    
+      const disconnectFromWebSocket = () => {
+        if (stomp) {
+          stomp.disconnect();
+        }
+      };
+    
+      connectToWebSocket();
+    
+      return disconnectFromWebSocket;
+    },[])
 
     useEffect(()=>{
         async function fetchContacts(){
@@ -21,17 +47,28 @@ export default function SideBar({username,token, id}){
             }
         }
         fetchContacts()
-    },[token])
+        if (stompClient) {
+            stompClient.subscribe(`/user/${username}/queue/notifications`, handleReceivedMessage);
+          }
+    },[token,stompClient,username])
+
+    const handleReceivedMessage = (message) => {
+        const notification = JSON.parse(message.body);
+        setNotifications(notification);
+      };
 
 
     return <div className="sidebar-container text-light">
         <div className="sidebar-username">
             <h4 className="m-0">{username}</h4>
-            <button className="btn btn-secondary"><FontAwesomeIcon icon={faHouse} size="xs" /></button>    
+            <button className="home-btn" onClick={()=>navigate("/")}><FontAwesomeIcon icon={faHouse} size="xs" /></button>    
         </div>
-        
+
         <div className="sidebar-chats">
-            {contacts?.map((contact,index)=><div className={`contact ${parseInt(id)===contact.id ? "active" : ""}`} key={index} onClick={()=>navigate(`/chat/${contact.id}`)}>{contact.username}</div>)}
+            {contacts?.map((contact,index)=><div className={`contact ${parseInt(id)===contact.id ? "active" : ""}`} key={index} onClick={()=>{navigate(`/chat/${contact.id}`);setNotifications(null)}}>
+                                            {contact.username}
+                                            {notification?.senderUsername === contact.username && <div className="in-chat notification-bubble"></div>}
+                                            </div>)}
         </div>
     </div>
 }
